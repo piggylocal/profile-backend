2cf62a88448a3a4a6ea746017ffd5e29a2e1a6f6
Zikai Chen <natnstart@gmail.com>
2024-10-09 21:57:16 +0900

    Ex: Add m3u8 api

---
+++ routers/ex/watch.ts
@@ -0,0 +1,34 @@
+import passport from "passport";
+import StatusCodes from "http-status-codes";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import router from "../user";
+
+puppeteer.use(StealthPlugin());
+router.post(
+    "/m3u8",
+    passport.authenticate("jwt", {session: false, failWithError: true}),
+    async (req, res) => {
+        const url = req.body.url;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const timeoutId = setTimeout(async () => {
+            await browser.close();
+            res.status(StatusCodes.BAD_REQUEST).json({message: "Timeout"});
+        }, 5000);
+        page.on('request', async request => {
+            if (request.url().trim().endsWith(".m3u8")) {
+                await browser.close();
+                clearTimeout(timeoutId);
+                res.status(StatusCodes.OK).json({url: request.url()});
+            }
+        });
+        try {
+            await page.goto(url);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+);
+
+export default router;
