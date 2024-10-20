d4666b352d15da14e468fa7574275994cad70c55
Zikai Chen <natnstart@gmail.com>
2024-10-20 22:25:59 +0900

    Feat: Add google auth refresh token.

--- routers/auth.ts
+++ routers/auth.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import {OAuth2Client} from "google-auth-library";
+import {OAuth2Client, UserRefreshClient} from "google-auth-library";
+import StatusCodes from "http-status-codes";
 
 const router = express.Router();
 const googleClient = new OAuth2Client(
@@ -12,5 +13,19 @@ router.post('/google', async (req, res) => {
     const {tokens} = await googleClient.getToken(req.body.code);
     res.json(tokens);
 });
+
+router.post('/google/refresh-token', async (req, res) => {
+    const {refreshToken} = req.body;
+    if (!refreshToken) {
+        return res.status(StatusCodes.BAD_REQUEST).json({message: "Refresh token is required"});
+    }
+    const user = new UserRefreshClient(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        req.body.refreshToken,
+    );
+    const {credentials} = await user.refreshAccessToken();
+    res.json(credentials);
+})
 
 export default router;
